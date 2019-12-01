8b9937d2fb8c813d16d7123fa97dbae66e3fb790
RFBerthes <rfbds102030@gmail.com>
2019-12-01 01:49:50 -0300

    criação classe convidado

---
+++ app/src/main/java/com/mobile/guestlist/Constantes.java
@@ -0,0 +1,10 @@
+package com.mobile.guestlist;
+
+public class Constantes {
+
+    public static class CONFIRMACAO {
+        public static final int CONFIRMADO = 1;
+        public static final int PENDENTE = 2;
+        public static final int NEGADO = 3;
+    }
+}
--- app/src/main/java/com/mobile/guestlist/FormularioActivity.java
+++ app/src/main/java/com/mobile/guestlist/FormularioActivity.java
@@ -3,12 +3,63 @@ package com.mobile.guestlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
-public class FormularioActivity extends AppCompatActivity {
+public class FormularioActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        this.mViewHolder.mEtNome = this.findViewById(R.id.etNome);
+        this.mViewHolder.mrbConfirm = this.findViewById(R.id.rbConfirmado);
+        this.mViewHolder.mrbPendente = this.findViewById(R.id.rbPendente);
+        this.mViewHolder.mrbNegado = this.findViewById(R.id.rbNegado);
+        this.mViewHolder.mbtnSalvar = this.findViewById(R.id.btnSalvar);
+
+        this.setListeners();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnSalvar){
+            this.salvar();
+        }
+    }
+
+    private void setListeners() {
+        this.mViewHolder.mbtnSalvar.setOnClickListener(this);
+    }
+
+
+    private void salvar() {
+
+        Convidado c = new Convidado();
+        c.setNome(this.mViewHolder.mEtNome.getText().toString());
+
+        if (this.mViewHolder.mrbConfirm.isChecked()){
+            c.setStatus(Constantes.CONFIRMACAO.CONFIRMADO);
+        }else if (this.mViewHolder.mrbPendente.isChecked()){
+            c.setStatus(Constantes.CONFIRMACAO.PENDENTE);
+        }else{
+            c.setStatus(Constantes.CONFIRMACAO.NEGADO);
+        }
+
+    }
+
+    private static class  ViewHolder {
+        EditText mEtNome;
+        RadioButton mrbConfirm;
+        RadioButton mrbPendente;
+        RadioButton mrbNegado;
+        Button mbtnSalvar;
+
     }
 }
